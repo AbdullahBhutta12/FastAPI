065f264ab8a84481859a96d7d25752200cb6bf18
AbdullahBhutta12 <116abdullahbhutta@gmail.com>
2025-08-25 20:00:57 +0500

    Response model, Create user and Hash password

--- blog/main.py
+++ blog/main.py
@@ -4,6 +4,8 @@ from blog import models
 from blog.database import engine, SessionLocal
 from blog import schemas
 from sqlalchemy.orm import Session
+from passlib.context import CryptContext
+# from typing import List
 
 models.Base.metadata.create_all(engine)
 app = FastAPI()
@@ -28,21 +30,25 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
 
 @app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
 def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(id == models.Blog.id).\
-        update({models.Blog.title:models.Blog.title - request}, synchronize_session=False)
-    db.query(models.Blog).filter(id == models.Blog.id).\
-        update({"title": models.Blog.title - request}, synchronize_session="evaluate")
+    blog = db.query(models.Blog).filter(id == models.Blog.id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+    blog.update(
+        {
+            "title": request.title,
+            "body": request.body
+        }, synchronize_session=False)
     db.commit()
     return 'updated'
 
 
-@app.get('/blog')
+@app.get('/blog', response_model=list[schemas.ShowBlog])
 def all(db: Session = Depends(get_db)):
     blog = db.query(models.Blog).all()
     return blog
 
 
-@app.get('/blog/{id}', status_code=200)
+@app.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog)
 def show(id: int, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(id == models.Blog.id).first()
     if not blog:
@@ -54,9 +60,23 @@ def show(id: int, response: Response, db: Session = Depends(get_db)):
 
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete_blog(id: int, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(id == models.Blog.id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(id == models.Blog.id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+    blog.delete(synchronize_session=False)
     db.commit()
     return 'done'
+
+pwd_cxt = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+@app.post('/user')
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    hashedPassword = pwd_cxt.hash(request.password)
+    new_user = models.User(name=request.name,email=request.email,password=hashedPassword)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
 
 # if __name__ == "__main__":
 #     uvicorn.run(app, host="0.0.0.0", port=9000, reload=True)
--- blog/schemas.py
+++ blog/schemas.py
@@ -1,6 +1,20 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class Blog(BaseModel):
     title: str
     body: str
+
+
+class ShowBlog(Blog):
+    title: str
+    body: str
+    # class Config:
+    #     orm_mode = True
+    model_config = ConfigDict(from_attributes=True)
+
+
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
