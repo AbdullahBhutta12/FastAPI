adef3ddcd2561144343b6b21210ee8584963eda5
AbdullahBhutta12 <116abdullahbhutta@gmail.com>
2025-08-28 18:22:39 +0500

    Login and verify

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -2,15 +2,19 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from blog import schemas, models
 from sqlalchemy.orm import Session
 from blog.database import *
-
+from blog.hashing import Hash
 router = APIRouter(
     tags=['authentication']
 )
 
 
-@router.get('/login')
-def login(request: schemas.login, db: Session = Depends(get_db())):
-    user = db.query(models.User).filter(request.username == models.User.email)
+@router.post('/login')
+def login(request: schemas.login, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(request.username == models.User.email).first()
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail="Invalid Credentials")
+    if Hash.verify(user.password, request.password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Incorrect password")
     return user
